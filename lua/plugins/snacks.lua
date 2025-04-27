a6665412e654989ad90f4ab07b85a02e9cb78a9c
jenkin <jenkinpan0628@gmail.com>
2025-04-27 16:19:40 +0800

    init git for neovim config

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,112 @@
+local M = require("plugins.mini")
+return {
+	{
+		"folke/snacks.nvim",
+		priority = 1000,
+		lazy = false,
+		opts = {
+			bigfile = { enabled = true },
+			dashboard = { enabled = true },
+			explorer = { enabled = true },
+			indent = { enabled = true },
+			input = { enabled = true },
+			picker = { enabled = true },
+			notifier = { enabled = true },
+			quickfile = { enabled = true },
+			scope = { enabled = true },
+			scroll = { enabled = true },
+			statuscolumn = { enabled = true },
+			words = { enabled = true },
+		},
+	},
+	{
+		"folke/snacks.nvim",
+		opts = {
+			dashboard = {
+				width = 60,
+				row = nil, -- dashboard position. nil for center
+				col = nil, -- dashboard position. nil for center
+				pane_gap = 4, -- empty columns between vertical panes
+				autokeys = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ", -- autokey sequence
+				preset = {
+					pick = nil,
+					keys = {
+						{
+							icon = " ",
+							key = "f",
+							desc = "Find File",
+							action = ":lua Snacks.dashboard.pick('files')",
+						},
+						{ icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+						{
+							icon = " ",
+							key = "g",
+							desc = "Find Text",
+							action = ":lua Snacks.dashboard.pick('live_grep')",
+						},
+						{
+							icon = " ",
+							key = "r",
+							desc = "Recent Files",
+							action = ":lua Snacks.dashboard.pick('oldfiles')",
+						},
+						{
+							icon = " ",
+							key = "c",
+							desc = "Config",
+							action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})",
+						},
+						{ icon = " ", key = "s", desc = "Restore Session", section = "session" },
+						{
+							icon = "󰒲 ",
+							key = "l",
+							desc = "Lazy",
+							action = ":Lazy",
+							enabled = package.loaded.lazy ~= nil,
+						},
+						{ icon = " ", key = "q", desc = "Quit", action = ":qa" },
+					},
+					-- Used by the `header` section
+					header = [[
+███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗
+████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║
+██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║
+██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║
+██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║
+╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝]],
+				},
+				-- item field formatters
+				formats = {
+					icon = function(item)
+						if item.file and item.icon == "file" or item.icon == "directory" then
+							return M.icon(item.file, item.icon)
+						end
+						return { item.icon, width = 2, hl = "icon" }
+					end,
+					footer = { "%s", align = "center" },
+					header = { "%s", align = "center" },
+					file = function(item, ctx)
+						local fname = vim.fn.fnamemodify(item.file, ":~")
+						fname = ctx.width and #fname > ctx.width and vim.fn.pathshorten(fname) or fname
+						if #fname > ctx.width then
+							local dir = vim.fn.fnamemodify(fname, ":h")
+							local file = vim.fn.fnamemodify(fname, ":t")
+							if dir and file then
+								file = file:sub(-(ctx.width - #dir - 2))
+								fname = dir .. "/…" .. file
+							end
+						end
+						local dir, file = fname:match("^(.*)/(.+)$")
+						return dir and { { dir .. "/", hl = "dir" }, { file, hl = "file" } }
+							or { { fname, hl = "file" } }
+					end,
+				},
+				sections = {
+					{ section = "header" },
+					{ section = "keys", gap = 1, padding = 1 },
+					{ section = "startup" },
+				},
+			},
+		},
+	},
+}
---
+++ lua/plugins/ui.lua
@@ -0,0 +1,467 @@
+return {
+	{
+		-- set UI: lualine(status line)
+		"nvim-lualine/lualine.nvim",
+		event = "VeryLazy",
+		init = function()
+			vim.g.lualine_laststatus = vim.o.laststatus
+			if vim.fn.argc(-1) > 0 then
+				-- set an empty statusline till lualine loads
+				vim.o.statusline = " "
+			else
+				-- hide the statusline on the starter page
+				vim.o.laststatus = 0
+			end
+		end,
+		opts = function()
+			-- PERF: we don't need this lualine require madness 🤷
+			local lualine_require = require("lualine_require")
+			local Snacks = require("snacks")
+			lualine_require.require = require
+
+			local icons = {
+				misc = {
+					dots = "󰇘",
+				},
+				ft = {
+					octo = "",
+				},
+				dap = {
+					Stopped = { "󰁕 ", "DiagnosticWarn", "DapStoppedLine" },
+					Breakpoint = " ",
+					BreakpointCondition = " ",
+					BreakpointRejected = { " ", "DiagnosticError" },
+					LogPoint = ".>",
+				},
+				diagnostics = {
+					Error = " ",
+					Warn = " ",
+					Hint = " ",
+					Info = " ",
+				},
+				git = {
+					added = " ",
+					modified = " ",
+					removed = " ",
+				},
+				kinds = {
+					Array = " ",
+					Boolean = "󰨙 ",
+					Class = " ",
+					Codeium = "󰘦 ",
+					Color = " ",
+					Control = " ",
+					Collapsed = " ",
+					Constant = "󰏿 ",
+					Constructor = " ",
+					Copilot = " ",
+					Enum = " ",
+					EnumMember = " ",
+					Event = " ",
+					Field = " ",
+					File = " ",
+					Folder = " ",
+					Function = "󰊕 ",
+					Interface = " ",
+					Key = " ",
+					Keyword = " ",
+					Method = "󰊕 ",
+					Module = " ",
+					Namespace = "󰦮 ",
+					Null = " ",
+					Number = "󰎠 ",
+					Object = " ",
+					Operator = " ",
+					Package = " ",
+					Property = " ",
+					Reference = " ",
+					Snippet = "󱄽 ",
+					String = " ",
+					Struct = "󰆼 ",
+					Supermaven = " ",
+					TabNine = "󰏚 ",
+					Text = " ",
+					TypeParameter = " ",
+					Unit = " ",
+					Value = " ",
+					Variable = "󰀫 ",
+				},
+			}
+
+			vim.o.laststatus = vim.g.lualine_laststatus
+
+			local opts = {
+				options = {
+					theme = "auto",
+					globalstatus = vim.o.laststatus == 3,
+					disabled_filetypes = { statusline = { "dashboard", "alpha", "ministarter", "snacks_dashboard" } },
+				},
+				sections = {
+					lualine_a = { "mode" },
+					lualine_b = { "branch" },
+
+					lualine_c = {
+						{
+							"diagnostics",
+							symbols = {
+								error = icons.diagnostics.Error,
+								warn = icons.diagnostics.Warn,
+								info = icons.diagnostics.Info,
+								hint = icons.diagnostics.Hint,
+							},
+						},
+						{ "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
+					},
+					lualine_x = {
+						Snacks.profiler.status(),
+            -- stylua: ignore
+            {
+              function() return require("noice").api.status.command.get() end,
+              cond = function() return package.loaded["noice"] and require("noice").api.status.command.has() end,
+              color = function() return { fg = Snacks.util.color("Statement") } end,
+            },
+            -- stylua: ignore
+            {
+              function() return require("noice").api.status.mode.get() end,
+              cond = function() return package.loaded["noice"] and require("noice").api.status.mode.has() end,
+              color = function() return { fg = Snacks.util.color("Constant") } end,
+            },
+            -- stylua: ignore
+            {
+              function() return "  " .. require("dap").status() end,
+              cond = function() return package.loaded["dap"] and require("dap").status() ~= "" end,
+              color = function() return { fg = Snacks.util.color("Debug") } end,
+            },
+            -- stylua: ignore
+            {
+              require("lazy.status").updates,
+              cond = require("lazy.status").has_updates,
+              color = function() return { fg = Snacks.util.color("Special") } end,
+            },
+						{
+							"diff",
+							symbols = {
+								added = icons.git.added,
+								modified = icons.git.modified,
+								removed = icons.git.removed,
+							},
+							source = function()
+								local gitsigns = vim.b.gitsigns_status_dict
+								if gitsigns then
+									return {
+										added = gitsigns.added,
+										modified = gitsigns.changed,
+										removed = gitsigns.removed,
+									}
+								end
+							end,
+						},
+					},
+					lualine_y = {
+						{ "progress", separator = " ", padding = { left = 1, right = 0 } },
+						{ "location", padding = { left = 0, right = 1 } },
+					},
+					lualine_z = {
+						function()
+							return " " .. os.date("%R")
+						end,
+					},
+				},
+				extensions = { "neo-tree", "lazy", "fzf" },
+			}
+
+			-- do not add trouble symbols if aerial is enabled
+			-- And allow it to be overriden for some buffer types (see autocmds)
+			if vim.g.trouble_lualine then
+				local trouble = require("trouble")
+				local symbols = trouble.statusline({
+					mode = "symbols",
+					groups = {},
+					title = false,
+					filter = { range = true },
+					format = "{kind_icon}{symbol.name:Normal}",
+					hl_group = "lualine_c_normal",
+				})
+				table.insert(opts.sections.lualine_c, {
+					symbols and symbols.get,
+					cond = function()
+						return vim.b.trouble_lualine ~= false and symbols.has()
+					end,
+				})
+			end
+
+			return opts
+		end,
+	},
+	{
+		-- set UI: Undo Tree
+		"mbbill/undotree",
+		event = "VeryLazy",
+	},
+	{
+		-- set UI: comment show
+		"folke/todo-comments.nvim",
+		event = "VeryLazy",
+		opts = {},
+	},
+	{
+		-- set UI: highlight colors
+		"brenoprata10/nvim-highlight-colors",
+		lazy = true,
+		event = "VeryLazy",
+		config = function()
+			require("nvim-highlight-colors").setup({
+				---Render style
+				---@usage 'background'|'foreground'|'virtual'
+				render = "background",
+
+				---Set virtual symbol (requires render to be set to 'virtual')
+				virtual_symbol = "■",
+
+				---Set virtual symbol suffix (defaults to '')
+				virtual_symbol_prefix = "",
+
+				---Set virtual symbol suffix (defaults to ' ')
+				virtual_symbol_suffix = " ",
+
+				---Set virtual symbol position()
+				---@usage 'inline'|'eol'|'eow'
+				---inline mimics VS Code style
+				---eol stands for `end of column` - Recommended to set `virtual_symbol_suffix = ''` when used.
+				---eow stands for `end of word` - Recommended to set `virtual_symbol_prefix = ' ' and virtual_symbol_suffix = ''` when used.
+				virtual_symbol_position = "inline",
+
+				---Highlight hex colors, e.g. '#FFFFFF'
+				enable_hex = true,
+
+				---Highlight short hex colors e.g. '#fff'
+				enable_short_hex = true,
+
+				---Highlight rgb colors, e.g. 'rgb(0 0 0)'
+				enable_rgb = true,
+
+				---Highlight hsl colors, e.g. 'hsl(150deg 30% 40%)'
+				enable_hsl = true,
+
+				---Highlight CSS variables, e.g. 'var(--testing-color)'
+				enable_var_usage = true,
+
+				---Highlight named colors, e.g. 'green'
+				enable_named_colors = true,
+
+				---Highlight tailwind colors, e.g. 'bg-blue-500'
+				enable_tailwind = true,
+
+				---Set custom colors
+				---Label must be properly escaped with '%' to adhere to `string.gmatch`
+				--- :help string.gmatch
+				custom_colors = {
+					{ label = "%-%-theme%-primary%-color", color = "#0f1219" },
+					{ label = "%-%-theme%-secondary%-color", color = "#5a5d64" },
+				},
+
+				-- Exclude filetypes or buftypes from highlighting e.g. 'exclude_buftypes = {'text'}'
+				exclude_filetypes = {},
+				exclude_buftypes = {},
+			})
+		end,
+	},
+	{
+		-- set UI: indent-blankline
+		"lukas-reineke/indent-blankline.nvim",
+		event = "VeryLazy",
+		main = "ibl",
+		opts = {
+			indent = {
+				char = "┊",
+				tab_char = "┊",
+			},
+			scope = { enabled = false },
+			exclude = {
+				filetypes = {
+					"help",
+					"alpha",
+					"dashboard",
+					"neo-tree",
+					"Trouble",
+					"trouble",
+					"lazy",
+					"mason",
+					"notify",
+					"toggleterm",
+					"lazyterm",
+				},
+			},
+		},
+	},
+	{
+		-- set UI: Buffer
+		"akinsho/bufferline.nvim",
+		event = "VeryLazy",
+		version = "*",
+		config = function()
+			vim.opt.termguicolors = true
+			require("bufferline").setup({
+				options = {
+					indicator = {
+						icon = "▎",
+						style = "icon",
+					},
+					diagnostics = "nvim_lsp",
+
+					diagnostics_indicator = function(count, level)
+						local icon = level:match("error") and " " or ""
+						return " " .. icon .. count
+					end,
+
+					offsets = {
+						{
+							filetype = "neo-tree",
+							text = "File Explorer",
+							text_align = "center",
+							separator = true,
+						},
+						{
+							filetype = "undotree",
+							text = "Undo Tree",
+							text_align = "center",
+							separator = true,
+						},
+						{
+							filetype = "NvimTree",
+							text = "File Explorer",
+							text_align = "center",
+							separator = true,
+						},
+					},
+					show_close_icon = false,
+					hover = {
+						enabled = true,
+						delay = 200,
+						reveal = { "close" },
+					},
+				},
+			})
+			vim.api.nvim_create_autocmd({ "BufAdd", "BufDelete" }, {
+				callback = function()
+					vim.schedule(function()
+						---@diagnostic disable-next-line: undefined-global
+						pcall(nvim_bufferline)
+					end)
+				end,
+			})
+		end,
+	},
+	{
+		-- set folds with nvim-ufo
+		"kevinhwang91/nvim-ufo",
+		event = "VeryLazy",
+		dependencies = {
+			"kevinhwang91/promise-async",
+		},
+		config = function()
+			local handler = function(virtText, lnum, endLnum, width, truncate)
+				local newVirtText = {}
+				local suffix = (" 󰁂 %d "):format(endLnum - lnum)
+				local sufWidth = vim.fn.strdisplaywidth(suffix)
+				local targetWidth = width - sufWidth
+				local curWidth = 0
+				for _, chunk in ipairs(virtText) do
+					local chunkText = chunk[1]
+					local chunkWidth = vim.fn.strdisplaywidth(chunkText)
+					if targetWidth > curWidth + chunkWidth then
+						table.insert(newVirtText, chunk)
+					else
+						chunkText = truncate(chunkText, targetWidth - curWidth)
+						local hlGroup = chunk[2]
+						table.insert(newVirtText, { chunkText, hlGroup })
+						chunkWidth = vim.fn.strdisplaywidth(chunkText)
+
+						if curWidth + chunkWidth < targetWidth then
+							suffix = suffix .. (" "):rep(targetWidth - curWidth - chunkWidth)
+						end
+						break
+					end
+					curWidth = curWidth + chunkWidth
+				end
+				table.insert(newVirtText, { suffix, "MoreMsg" })
+				return newVirtText
+			end
+
+			require("ufo").setup({
+				fold_virt_text_handler = handler,
+				provider_selector = function()
+					return { "treesitter", "indent" }
+				end,
+			})
+		end,
+	},
+	{ "MunifTanjim/nui.nvim", lazy = true },
+	{ "rcarriga/nvim-notify", lazy = true },
+	{
+		-- set UI for notification, cmdline and popupmenu
+		"folke/noice.nvim",
+		event = "VeryLazy",
+		config = function()
+			require("noice").setup({
+				lsp = {
+					override = {
+						["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+						["vim.lsp.util.stylize_markdown"] = true,
+						["cmp.entry.get_documentation"] = true,
+					},
+				},
+				routes = {
+					filter = {
+						{
+							view = "notify",
+							filter = { event = "msg_showmode" },
+						},
+						{
+							event = "notify",
+							find = "No information available",
+						},
+					},
+					opts = { skip = true },
+				},
+				views = {
+					-- set cmdline_popup and popupmenu position
+					cmdline_popup = {
+						position = {
+							row = "50%",
+							col = "50%",
+						},
+						size = {
+							width = 60,
+							height = "auto",
+						},
+					},
+					popupmenu = {
+						position = {
+							row = "71%",
+							col = "50%",
+						},
+						size = {
+							width = 60,
+							height = 10,
+						},
+						border = {
+							style = "rounded",
+							padding = { 0, 1 },
+						},
+						win_options = {
+							winhighlight = { Normal = "Normal", FloatBorder = "DiagnosticInfo" },
+						},
+					},
+				},
+				presets = {
+					bottom_search = true,
+					command_palette = false,
+					long_message_to_split = true,
+					inc_rename = false,
+					lsp_doc_border = false,
+				},
+			})
+		end,
+	},
+}
